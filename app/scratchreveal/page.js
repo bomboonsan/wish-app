11a9951a8513e9fde77d5f6737f55bfeeb60dd26
bomboonsan <bomboonsan@gmail.com>
2025-05-23 16:02:10 +0700

    update

--- app/scratchreveal/page.js
+++ app/scratchreveal/page.js
@@ -4,9 +4,10 @@ import ScratchReveal from '@/components/ScratchReveal';
 export default function ScratchRevealPage() {
 
   return (
-    <main className='max-w-[800px] bg-white mx-auto shadow-md min-h-screen'>
-      <div className='text-center mb-10 pt-20'>
+    <main className=' bg-white mx-auto min-h-screen'>
+      <div className='text-center mb-10 pt-20 flex items-center justify-center'>
         <h1 className='text-2xl font-extrabold text-[#33BDED]'>Scratch To Reveal</h1>
+        <img src="/eraser.gif" className='w-14 h-14' alt="" />
       </div>
       <div id="canvasWrapper" className='' style={{ position: "relative" }}>
         <ScratchReveal />
